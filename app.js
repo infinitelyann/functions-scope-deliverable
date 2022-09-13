63131bdc26e635478625a50a152d594a7b457075
saparker <stephanieparker1997@gmail.com>
2022-09-12 21:35:23 -0400

    Commit 1 - Verbal Questions

---
+++ app.js
@@ -0,0 +1,9 @@
+ Question #1// Parameter goes in the function() when you declare the function-
+// Argument goes in the function() when you  call the function 
+
+Question #2// console.log() is a function that will display an object and its values in the console when passed into () 
+
+// return is used to reply a value back to the invocation source 
+
+Question #3//   a function sets actions and conditions to be performed on/determined for a variable that is put within it's () -parameters-
+// a function implies that when a variable is placed in its parameters, it now becomes an arugument, to which the function will perform it's action upon 
